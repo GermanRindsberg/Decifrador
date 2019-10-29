67cf969cae273177d0f8eaaeb8bef8f0d2d0abfa
German <rindsberg@hotmail.com>
2019-10-29 14:12:45 -0300

    segundo commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMensajeIncorrectos = new System.Windows.Forms.Label();
+            this.lblMensajeCorrectos = new System.Windows.Forms.Label();
+            this.btnProbar = new System.Windows.Forms.Button();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.txtNum4 = new System.Windows.Forms.TextBox();
+            this.txtNum3 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblMensajeIncorrectos
+            // 
+            this.lblMensajeIncorrectos.AutoSize = true;
+            this.lblMensajeIncorrectos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensajeIncorrectos.ForeColor = System.Drawing.Color.White;
+            this.lblMensajeIncorrectos.Location = new System.Drawing.Point(23, 328);
+            this.lblMensajeIncorrectos.Name = "lblMensajeIncorrectos";
+            this.lblMensajeIncorrectos.Size = new System.Drawing.Size(58, 24);
+            this.lblMensajeIncorrectos.TabIndex = 23;
+            this.lblMensajeIncorrectos.Text = "Texto";
+            this.lblMensajeIncorrectos.Visible = false;
+            // 
+            // lblMensajeCorrectos
+            // 
+            this.lblMensajeCorrectos.AutoSize = true;
+            this.lblMensajeCorrectos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMensajeCorrectos.ForeColor = System.Drawing.Color.White;
+            this.lblMensajeCorrectos.Location = new System.Drawing.Point(24, 291);
+            this.lblMensajeCorrectos.Name = "lblMensajeCorrectos";
+            this.lblMensajeCorrectos.Size = new System.Drawing.Size(58, 24);
+            this.lblMensajeCorrectos.TabIndex = 22;
+            this.lblMensajeCorrectos.Text = "Texto";
+            this.lblMensajeCorrectos.Visible = false;
+            // 
+            // btnProbar
+            // 
+            this.btnProbar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProbar.Location = new System.Drawing.Point(269, 222);
+            this.btnProbar.Name = "btnProbar";
+            this.btnProbar.Size = new System.Drawing.Size(85, 44);
+            this.btnProbar.TabIndex = 21;
+            this.btnProbar.Text = "Probar";
+            this.btnProbar.UseVisualStyleBackColor = true;
+            this.btnProbar.Visible = false;
+            // 
+            // txt4
+            // 
+            this.txt4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt4.Location = new System.Drawing.Point(295, 162);
+            this.txt4.MaxLength = 1;
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(59, 44);
+            this.txt4.TabIndex = 20;
+            this.txt4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt4.Visible = false;
+            // 
+            // txt3
+            // 
+            this.txt3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt3.Location = new System.Drawing.Point(206, 162);
+            this.txt3.MaxLength = 1;
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(59, 44);
+            this.txt3.TabIndex = 19;
+            this.txt3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt3.Visible = false;
+            // 
+            // txt2
+            // 
+            this.txt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.Location = new System.Drawing.Point(117, 162);
+            this.txt2.MaxLength = 1;
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(59, 44);
+            this.txt2.TabIndex = 18;
+            this.txt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt2.Visible = false;
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(28, 162);
+            this.txt1.MaxLength = 1;
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(59, 44);
+            this.txt1.TabIndex = 17;
+            this.txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt1.Visible = false;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.Location = new System.Drawing.Point(89, 384);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(197, 42);
+            this.btnIniciar.TabIndex = 16;
+            this.btnIniciar.Text = "INICIAR";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click_1);
+            // 
+            // txtNum4
+            // 
+            this.txtNum4.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum4.Location = new System.Drawing.Point(294, 26);
+            this.txtNum4.Name = "txtNum4";
+            this.txtNum4.Size = new System.Drawing.Size(59, 116);
+            this.txtNum4.TabIndex = 15;
+            this.txtNum4.Visible = false;
+            // 
+            // txtNum3
+            // 
+            this.txtNum3.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum3.Location = new System.Drawing.Point(205, 26);
+            this.txtNum3.Name = "txtNum3";
+            this.txtNum3.Size = new System.Drawing.Size(59, 116);
+            this.txtNum3.TabIndex = 14;
+            this.txtNum3.Visible = false;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum2.Location = new System.Drawing.Point(116, 26);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(59, 116);
+            this.txtNum2.TabIndex = 13;
+            this.txtNum2.Visible = false;
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum1.Location = new System.Drawing.Point(27, 26);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(59, 116);
+            this.txtNum1.TabIndex = 12;
+            this.txtNum1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(378, 439);
+            this.Controls.Add(this.lblMensajeIncorrectos);
+            this.Controls.Add(this.lblMensajeCorrectos);
+            this.Controls.Add(this.btnProbar);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.txtNum4);
+            this.Controls.Add(this.txtNum3);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMensajeIncorrectos;
+        private System.Windows.Forms.Label lblMensajeCorrectos;
+        private System.Windows.Forms.Button btnProbar;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.TextBox txtNum4;
+        private System.Windows.Forms.TextBox txtNum3;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtNum1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            Random ran = new Random();
+
+            int num1 = ran.Next(1, 10);
+            int num2 = ran.Next(1, 10);
+            int num3 = ran.Next(1, 10);
+            int num4 = ran.Next(1, 10);
+
+            txtNum1.Text = Convert.ToString(num1);
+            txtNum2.Text = Convert.ToString(num2);
+            txtNum3.Text = Convert.ToString(num3);
+            txtNum4.Text = Convert.ToString(num4);
+
+            btnIniciar.Visible = false;
+            txtNum1.Visible = false;
+            txtNum2.Visible = false;
+            txtNum3.Visible = false;
+            txtNum4.Visible = false;
+            lblMensajeCorrectos.Text = "";
+            btnProbar.Visible = true;
+            txt1.Text = "";
+            txt2.Text = "";
+            txt3.Text = "";
+            txt4.Text = "";
+            txt1.Visible = true;
+            txt2.Visible = true;
+            txt3.Visible = true;
+            txt4.Visible = true;
+            txt1.Focus();
+
+        }
+
+        #region Efectos visuales
+
+        private void txt1_TextChanged(object sender, EventArgs e)
+        {
+            if (txt1.Text != "")
+            {
+                txt2.Focus();
+            }
+        }
+
+        private void txt2_TextChanged(object sender, EventArgs e)
+        {
+            if (txt2.Text != "")
+            {
+                txt3.Focus();
+            }
+        }
+
+        private void txt3_TextChanged(object sender, EventArgs e)
+        {
+            if (txt3.Text != "")
+            {
+                txt4.Focus();
+            }
+        }
+
+        private void txt4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Back))
+            {
+                txt4.Text = "";
+                txt3.Focus();
+            }
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                String num1 = txt1.Text;
+                String num2 = txt2.Text;
+                String num3 = txt3.Text;
+                String num4 = txt4.Text;
+                int contadorCorrectos = 0;
+                int contadorIncorrectos = 0;
+
+                if (num1 == txtNum1.Text || num1 == txtNum2.Text || num1 == txtNum3.Text || num1 == txtNum4.Text)
+                {
+                    if (num1 == txtNum1.Text && num1 != txtNum2.Text && num1 != txtNum3.Text && num1 != txtNum4.Text)
+                    {
+                        contadorCorrectos++;
+                    }
+                    else
+                    {
+                        contadorIncorrectos++;
+                    }
+                }
+
+                if (num2 == txtNum1.Text || num2 == txtNum2.Text || num2 == txtNum3.Text || num2 == txtNum4.Text)
+                {
+                    if (num2 == txtNum2.Text && num2 != txtNum1.Text && num2 != txtNum3.Text && num2 != txtNum4.Text)
+                    {
+                        contadorCorrectos++;
+                    }
+                    else
+                    {
+                        contadorIncorrectos++;
+                    }
+                }
+
+                if (num3 == txtNum1.Text || num3 == txtNum2.Text || num3 == txtNum3.Text || num3 == txtNum4.Text)
+                {
+                    if (num3 == txtNum3.Text && num3 != txtNum2.Text && num3 != txtNum1.Text && num3 != txtNum4.Text)
+                    {
+                        contadorCorrectos++;
+                    }
+                    else
+                    {
+                        contadorIncorrectos++;
+                    }
+                }
+
+                if (num4 == txtNum1.Text || num4 == txtNum2.Text || num4 == txtNum3.Text || num4 == txtNum4.Text)
+                {
+                    if (num4 == txtNum4.Text && num4 != txtNum2.Text && num4 != txtNum3.Text && num4 != txtNum1.Text)
+                    {
+                        contadorCorrectos++;
+                    }
+                    else
+                    {
+                        contadorIncorrectos++;
+                    }
+                }
+
+                lblMensajeCorrectos.Text = "Hay " + contadorCorrectos + " numeros correctos bien ubicados";
+                lblMensajeIncorrectos.Text = "Hay " + contadorIncorrectos + " numeros correctos mal ubicados";
+                lblMensajeCorrectos.Visible = true;
+                lblMensajeIncorrectos.Visible = true;
+
+                if (contadorCorrectos == 4)
+                {
+                    lblMensajeCorrectos.Text = "Felicitaciones, has ganado";
+                    lblMensajeIncorrectos.Visible = false;
+                    btnIniciar.Visible = true;
+                    btnProbar.Visible = false;
+                    txtNum1.Visible = true;
+                    txtNum2.Visible = true;
+                    txtNum3.Visible = true;
+                    txtNum4.Visible = true;
+                }
+            }
+
+
+        }
+
+        private void txt3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Back))
+            {
+                txt3.Text = "";
+                txt2.Focus();
+            }
+        }
+
+        private void txt2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Back))
+            {
+                txt2.Text = "";
+                txt1.Focus();
+            }
+        }
+
+        private void txt1_Enter(object sender, EventArgs e)
+        {
+            txt1.Text = "";
+        }
+
+        private void txt2_Enter(object sender, EventArgs e)
+        {
+            txt2.Text = "";
+        }
+
+        private void txt3_Enter(object sender, EventArgs e)
+        {
+            txt3.Text = "";
+        }
+
+        private void txt4_Enter(object sender, EventArgs e)
+        {
+            txt4.Text = "";
+        }
+
+
+
+        #endregion
+
+        private void btnProbar_Click(object sender, EventArgs e)
+        {
+            String num1 = txt1.Text;
+            String num2 = txt2.Text;
+            String num3 = txt3.Text;
+            String num4 = txt4.Text;
+            int contadorCorrectos = 0;
+            int contadorIncorrectos = 0;
+
+            if (num1 == txtNum1.Text || num1 == txtNum2.Text || num1 == txtNum3.Text || num1 == txtNum4.Text)
+            {
+                if (num1 == txtNum1.Text && num1 != txtNum2.Text && num1 != txtNum3.Text && num1 != txtNum4.Text)
+                {
+                    contadorCorrectos++;
+
+                }
+                else
+                {
+                    contadorIncorrectos++;
+                }
+            }
+
+            if (num2 == txtNum1.Text || num2 == txtNum2.Text || num2 == txtNum3.Text || num2 == txtNum4.Text)
+            {
+                if (num2 == txtNum2.Text && num2 != txtNum1.Text && num2 != txtNum3.Text && num2 != txtNum4.Text)
+                {
+                    contadorCorrectos++;
+
+                }
+                else
+                {
+                    contadorIncorrectos++;
+
+                }
+            }
+
+            if (num3 == txtNum1.Text || num3 == txtNum2.Text || num3 == txtNum3.Text || num3 == txtNum4.Text)
+            {
+                if (num3 == txtNum3.Text && num3 != txtNum2.Text && num3 != txtNum1.Text && num3 != txtNum4.Text)
+                {
+                    contadorCorrectos++;
+
+                }
+                else
+                {
+                    contadorIncorrectos++;
+                }
+            }
+
+            if (num4 == txtNum1.Text || num4 == txtNum2.Text || num4 == txtNum3.Text || num4 == txtNum4.Text)
+            {
+                if (num4 == txtNum4.Text && num4 != txtNum2.Text && num4 != txtNum3.Text && num4 != txtNum1.Text)
+                {
+                    contadorCorrectos++;
+
+                }
+                else
+                {
+                    contadorIncorrectos++;
+                }
+            }
+
+            lblMensajeCorrectos.Text = "Hay " + contadorCorrectos + " numeros correctos bien ubicados";
+            lblMensajeIncorrectos.Text = "Hay " + contadorIncorrectos + " numeros correctos mal ubicados";
+            lblMensajeCorrectos.Visible = true;
+            lblMensajeIncorrectos.Visible = true;
+
+            if (contadorCorrectos == 4)
+            {
+                lblMensajeCorrectos.Text = "Felicitaciones, has ganado";
+                lblMensajeIncorrectos.Visible = false;
+                btnIniciar.Visible = true;
+                btnProbar.Visible = false;
+                txtNum1.Visible = true;
+                txtNum2.Visible = true;
+                txtNum3.Visible = true;
+                txtNum4.Visible = true;
+
+            }
+
+        }
+
+        private void btnIniciar_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
